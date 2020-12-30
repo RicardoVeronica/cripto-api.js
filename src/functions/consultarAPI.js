cca42ed0ab908efcf93af28b433a72c76a7f927c
setcain <charlie.bronson00@gmail.com>
2020-12-30 08:30:34 -0600

    updates consultarAPI changes promise to asyncAwait

--- src/functions/consultarAPI.js
+++ src/functions/consultarAPI.js
@@ -2,17 +2,26 @@ import * as UI from "../selectors";
 import mostrarCotizacion from "./mostrarCotizacion";
 import mostrarSpinner from "./mostrarSpinner";
 
-function consultarAPI() {
+async function consultarAPI() {
+  // Consulta API usando fetch con async/await
   const { moneda, criptomoneda } = UI.objBusqueda;
   const url = `https://min-api.cryptocompare.com/data/pricemultifull?fsyms=${criptomoneda}&tsyms=${moneda}`;
 
   mostrarSpinner();
 
-  fetch(url)
-    .then((result) => result.json())
-    .then((cotizacion) =>
-      mostrarCotizacion(cotizacion.DISPLAY[criptomoneda][moneda])
-    );
+  // fetch(url)
+  //   .then((result) => result.json())
+  //   .then((cotizacion) =>
+  //     mostrarCotizacion(cotizacion.DISPLAY[criptomoneda][moneda])
+  //   );
+
+  try {
+    const resultado = await fetch(url);
+    const cotizacion = await resultado.json();
+    mostrarCotizacion(cotizacion.DISPLAY[criptomoneda][moneda]);
+  } catch (e) {
+    console.log(e);
+  }
 }
 
 export default consultarAPI;
